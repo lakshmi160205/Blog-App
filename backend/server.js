52296cd425b480938dd7f3d918736ffe9382ed45
lakshmi160205 <23eg112e38@anurag.edu.in>
2026-05-15 11:27:17 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,13 @@ const app = exp();
 //port 
 const PORT=process.env.PORT || 4000;
 //use cors middleware
-app.use(cors({ origin: ["http://localhost:5173", "https://blog-app-ashen-three.vercel.app"],credentials:true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://blog-gqpgu9pyl-lakshmi160205s-projects.vercel.app"
+  ],
+  credentials: true
+}));
 //add body parser middleware
 app.use(exp.json());
 //add cookie parser middleware
